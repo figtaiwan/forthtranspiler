30f72242605443c26e0eb9c9b074d2af663258d5
samsuanchen <samsuanchen@gmail.com>
2015-04-05 10:38:37 +0800

    add test swap drop rot -rot

--- test/test.js
+++ test/test.js
@@ -3,70 +3,82 @@ var Transpile	=require("../src/transpile.js");
 var transpile	=Transpile.transpile;
 
 Transpile.trace(true);
-describe(						"A.  test literals"
-	,function(){				///////////////////
+describe(			"A.  test literals"
+	,function(){		///////////////////
 
-	it(								"A1.  test integers ok"
+	it(				"A1.  test integers ok"
 	,function(){assert.deepEqual(	//////////////////////
-		transpile([					"0 1 23 -4 56789"
-		]).stack,					[0,1,23,-4,56789]
+		transpile([		"0 1 23 -4 56789"
+		]).stack,		[0,1,23,-4,56789]
 	)})
 
-	it(								"A2.  test floats ok"
+	it(				"A2.  test floats ok"
 	,function(){assert.deepEqual(	//////////////////////
-		transpile([					"0.1 -2.3 -4.5e6"
-		]).stack,					[0.1,-2.3,-4.5e6]
+		transpile([		"0.1 -2.3 -4.5e6"
+		]).stack,		[0.1,-2.3,-4.5e6]
 	)})
 
-	it(								"A3.  test strings ok"
+	it(				"A3.  test strings ok"
 	,function(){assert.deepEqual(	//////////////////////
-		transpile([					"'0.1' 'abc' 'deFghi'"
-		]).stack,					['0.1','abc','deFghi']
+		transpile([		"'0.1' 'abc' 'deFghi'"
+		]).stack,		['0.1','abc','deFghi']
 	)})
 
 });
 
-describe(						"B.  test core words"
-	,function(){				/////////////////////
+describe(			"B.  test core words"
+	,function(){		/////////////////////
 
-	it(								"B1.  test + numbers ok"
-	,function(){assert.deepEqual(	/////////////////////
-		transpile([					"3 5 +"
-		]).stack,					[8]
+	it(				"B1.  test + numbers ok"
+	,function(){assert.deepEqual(	////////////////////////
+		transpile([		"3 5 +"
+		]).stack,		[8]
 	)})
 
-	it(								"B2.  test - numbers ok"
-	,function(){ assert.deepEqual(	/////////////////////
-		transpile([					"5 3 -"
-		]).stack,					[2]
+	it(				"B2.  test - numbers ok"
+	,function(){ assert.deepEqual(	////////////////////////
+		transpile([		"5 3 -"
+		]).stack,		[2]
 	)})
 
-	it(								"B3.  test + strings ok"
-	,function(){ assert.deepEqual(	/////////////////////
-		transpile([					"'3' 5 +"
-		]).stack,					["35"]
+	it(				"B3.  test + strings ok"
+	,function(){ assert.deepEqual(	////////////////////////
+		transpile([		"'3' 5 +"
+		]).stack,		["35"]
 	)})
 
-	it(								"B4.  test dup * ok"
-	,function(){ assert.deepEqual(	///////////////
-		transpile([					"3.5 dup *"
-		]).stack,					[12.25]
+	it(				"B4.  test dup * ok"
+	,function(){ assert.deepEqual(	////////////////////
+		transpile([		"3.5 dup *"
+		]).stack,		[12.25]
+	)})
+
+	it(				"B5.  test swap drop ok"
+	,function(){ assert.deepEqual(	////////////////////
+		transpile([		"3 5 swap drop"
+		]).stack,		[5]
+	)})
+
+	it(				"B6.  test rot -rot ok"
+	,function(){ assert.deepEqual(	////////////////////
+		transpile([		"1 2 3 rot swap -rot"
+		]).stack,		[3 2 1]
 	)})
 
 });
 
-describe(						"C.  test defining words"
-	,function(){				/////////////////////
+describe(			"C.  test defining words"
+	,function(){		/////////////////////////
 
-	it(								"C1.  test value ok"
-	,function(){ assert.deepEqual(	/////////////////
-		transpile([					"5 value a 3 a +"
-		]).stack,					[8]
+	it(				"C1.  test value ok"
+	,function(){ assert.deepEqual(	////////////////////
+		transpile([		"5 value a 3 a +"
+		]).stack,		[8]
 	)})
 
-	it(								"C2.  test : ; ok"
-	,function(){ assert.deepEqual(	/////////////////
-		transpile([					": x 5 + ; 3 x"
-		]).stack,					[8]
+	it(				"C2.  test : ; ok"
+	,function(){ assert.deepEqual(	//////////////////
+		transpile([		": x 5 + ; 3 x"
+		]).stack,		[8]
 	)})
 });
